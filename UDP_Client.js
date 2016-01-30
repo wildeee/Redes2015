6e3d23223802430dea70cef304d7ee2aabd01089
Wilde <gutopicciani@yahoo.com.br>
2016-01-30 00:18:45 -0200

    Avanço na implementação do cliente

--- UDP_Client.js
+++ UDP_Client.js
@@ -15,47 +15,49 @@ var client1 = dgram.createSocket('udp4');
 client1.send(message1, 0, message1.length, PORT, HOST, function(err, bytes) {
     if (err) throw err;
     console.log('UDP message sent to ' + HOST +':'+ PORT);
+    console.log(err);
+    console.log(bytes)
     client1.close();
 });
 
-var client2 = dgram.createSocket('udp4');
-client2.send(message2, 0, message2.length, PORT, HOST, function(err, bytes) {
-    if (err) throw err;
-    console.log('UDP message sent to ' + HOST +':'+ PORT);
-    client2.close();
-});
+// var client2 = dgram.createSocket('udp4');
+// client2.send(message2, 0, message2.length, PORT, HOST, function(err, bytes) {
+//     if (err) throw err;
+//     console.log('UDP message sent to ' + HOST +':'+ PORT);
+//     client2.close();
+// });
 
-var client3 = dgram.createSocket('udp4');
-client3.send(message3, 0, message3.length, PORT, HOST, function(err, bytes) {
-    if (err) throw err;
-    console.log('UDP message sent to ' + HOST +':'+ PORT);
-    client3.close();
-});
+// var client3 = dgram.createSocket('udp4');
+// client3.send(message3, 0, message3.length, PORT, HOST, function(err, bytes) {
+//     if (err) throw err;
+//     console.log('UDP message sent to ' + HOST +':'+ PORT);
+//     client3.close();
+// });
 
-var client4 = dgram.createSocket('udp4');
-client4.send(message4, 0, message4.length, PORT, HOST, function(err, bytes) {
-    if (err) throw err;
-    console.log('UDP message sent to ' + HOST +':'+ PORT);
-    client4.close();
-});
+// var client4 = dgram.createSocket('udp4');
+// client4.send(message4, 0, message4.length, PORT, HOST, function(err, bytes) {
+//     if (err) throw err;
+//     console.log('UDP message sent to ' + HOST +':'+ PORT);
+//     client4.close();
+// });
 
-var client5 = dgram.createSocket('udp4');
-client5.send(message5, 0, message5.length, PORT, HOST, function(err, bytes) {
-    if (err) throw err;
-    console.log('UDP message sent to ' + HOST +':'+ PORT);
-    client5.close();
-});
+// var client5 = dgram.createSocket('udp4');
+// client5.send(message5, 0, message5.length, PORT, HOST, function(err, bytes) {
+//     if (err) throw err;
+//     console.log('UDP message sent to ' + HOST +':'+ PORT);
+//     client5.close();
+// });
 
-var client6 = dgram.createSocket('udp4');
-client6.send(message6, 0, message6.length, PORT, HOST, function(err, bytes) {
-    if (err) throw err;
-    console.log('UDP message sent to ' + HOST +':'+ PORT);
-    client6.close();
-});
+// var client6 = dgram.createSocket('udp4');
+// client6.send(message6, 0, message6.length, PORT, HOST, function(err, bytes) {
+//     if (err) throw err;
+//     console.log('UDP message sent to ' + HOST +':'+ PORT);
+//     client6.close();
+// });
 
-var client7 = dgram.createSocket('udp4');
-client7.send(message7, 0, message7.length, PORT, HOST, function(err, bytes) {
-    if (err) throw err;
-    console.log('UDP message sent to ' + HOST +':'+ PORT);
-    client7.close();
-});
+// var client7 = dgram.createSocket('udp4');
+// client7.send(message7, 0, message7.length, PORT, HOST, function(err, bytes) {
+//     if (err) throw err;
+//     console.log('UDP message sent to ' + HOST +':'+ PORT);
+//     client7.close();
+// });
--- UDP_Routes.js
+++ UDP_Routes.js
@@ -1,5 +1,19 @@
 module.exports = function(server){
 	server.on('message', function (message, remote) {
     	console.log(remote.address + ':' + remote.port +' - ' + message);
+
+    	var response = "caralho";
+    	server.send(
+            response,
+            0, // Buffer offset
+            response.length,
+            remote.port,
+            remote.address,
+            function( error, byteLength ) {
+
+                console.log( "... Sent response to " + remote.address + ":" + remote.port );
+
+            }
+        );
 	});
 };
